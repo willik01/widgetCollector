71534ad3bd72663a92b8d7ec6c7f45b4a13f4de2
Keith Williams (willik01) <me@keithwilliams.net>
2022-10-24 08:41:14 -0700

    Add basic views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# fake data for testing
+class Widget: 
+  def __init__(self, name, type, description, price):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.price = price
 
-# Create your views here.
+widgets = [
+  Widget('Thing 1', 'object', '6 sided cube', 3),
+  Widget('Thing 2', 'consumer item', 'fidget spinner', 5),
+  Widget('Thing 3', 'business item', 'pencil sharpener', 23)
+]
+
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+# Add new view
+def widgets_index(request):
+  return render(request, 'widgets/index.html', { 'widgets': widgets })
--- widgetcollector/settings.py
+++ widgetcollector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'widgetcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'widgetcollector',
+        # 'HOST': 'localhost',  <-- some computers might need this line
     }
 }
 
